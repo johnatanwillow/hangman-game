7dd06d8852775942b02cb6537f300a9e95d75853
ArthurMartins-9 <arthurmartinsbatista09@gmail.com>
2025-05-12 15:57:40 -0300

    Add files via upload

---
+++ first-version/ranking.h
@@ -0,0 +1,6 @@
+#pragma once
+
+void rankingEmVetor(char *nome, int *pSomaDasPontuacoes,int pontuacao_ante[],int *pI);
+int contaLinhas();
+void escreveArquivo(int tamanho);
+void ajustarPontuacao(char *nome , int pontuacao_player);
