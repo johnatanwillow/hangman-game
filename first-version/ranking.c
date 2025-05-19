a9ab41ac8616760593f6db2164c3e0279494ab00
ArthurMartins-9 <arthurmartinsbatista09@gmail.com>
2025-05-19 12:18:13 -0300

    Update ranking.c

--- first-version/ranking.c
+++ first-version/ranking.c
@@ -2,75 +2,29 @@
 #include <string.h>
 #include <stdlib.h>
 
-//---------------------------------------
-#include "ranking.h"
-//---------------------------------------
+#include "ranking.h"//Ta dentro o struct memoria e as funções
 
-#define MAX_N_CHAR 255
-#define MAX_PLAYERS 1000
+char buffer[MAX_N_CHAR];//Char global para ler os arquivos
 
-char buffer[MAX_N_CHAR];
-char memoria[MAX_PLAYERS][2][MAX_N_CHAR];
-
-void colocarNomeNoRanking(char *nome, int pontuacao_player)
+void ranqueamento(char *nome, int pontuacao_player)
 {
-
     int tamanho = contaLinhasRanking();
-    int pontuacao_players[tamanho];
-    int i = 0; // i é o indice,
+    int iNome= -1;     // iN é o lugar onde esteja a última pontuação do jogador.Se for -1 é pq esse nome é inédito
+    Memoria memoria[tamanho + 1];
 
-    int iN;     // iN é o lugar onde esteja a última pontuação do jogador.Se for -1 é pq esse nome é inédito
-    int s = -1; // Por algum motivo se for iN no seco muda no sprinft o valor dele, logo precisa desse segund valor para iN;
+    rankingEmVetor(nome, &pontuacao_player,&iNome,memoria);
+    
+    organizaRanking(nome,pontuacao_player,iNome,&tamanho,memoria);
 
-    int n = 1; // Se for inédito n irá aumentar o tamanho na hora de demsontrar o highscore.Se n for inédito ele será zero.
-    char pontuacao[2];
-    char aux1[2][MAX_N_CHAR], aux2[2][MAX_N_CHAR];
+    escreveArquivo(tamanho,memoria);
 
-    rankingEmVetor(nome, &pontuacao_player, pontuacao_players, &s);
-    iN = s;
-
-    while (pontuacao_player < pontuacao_players[i] && i < tamanho)
-        i++; // Se for maior agente ignora
-    // Quando acabar ou seja pela pontuação anterior ser menor que a atual ou o indice
-
-    // Esse blocão aqui é um porblema e agente precisa arrumar um jeito de diminuílo
-    for (int j = 0; j < 2; j++)
-        strncpy(aux1[j], memoria[i][j], MAX_N_CHAR - 1);
-    strncpy(memoria[i][0], nome, MAX_N_CHAR - 1);
-    sprintf(pontuacao, "%d", pontuacao_player);
-    strncpy(memoria[i][1], pontuacao, MAX_N_CHAR - 1);
-
-    while (i <= tamanho)
-    {
-        i++;
-        for (int j = 0; j < 2; j++)
-        {
-            strncpy(aux2[j], memoria[i][j], MAX_N_CHAR - 1);
-            strncpy(memoria[i][j], aux1[j], MAX_N_CHAR - 1);
-            strncpy(aux1[j], aux2[j], MAX_N_CHAR - 1);
-        }
-    }
-
-    if (iN != -1)
-    {
-        iN++;
-        strncpy(memoria[iN][0], memoria[iN + 1][0], MAX_N_CHAR - 1);
-        strncpy(memoria[iN][1], memoria[iN + 1][1], MAX_N_CHAR - 1);
-        n = 0;
-        for (int i = iN; i <= tamanho; i++)
-            for (int j = 0; j < 2; j++)
-            {
-                strncpy(memoria[i][j], memoria[i + 1][j], MAX_N_CHAR - 1);
-            }
-    }
-    tamanho += n;
-    escreveArquivo(tamanho);
-
-    printf("=============Highscores===============");
-    for (int j = 0; j < tamanho; j++)
-        printf("\n%d %s %s", j + 1, memoria[j][0], memoria[j][1]);
+    //Para debug do progrma utillize isso + main do rankning
+    //   printf("=============Highscores===============");
+    //  for (int i = 0; i < tamanho; i++)
+    //      printf("\n%d %s %s", i + 1, memoria[i].nome,memoria[i].pontuacao);
 }
-void rankingEmVetor(char *nome, int *pSomaDasPontuacoes, int pontuacao_ante[], int *pIn)
+
+void rankingEmVetor(char *nome, int *pSomaDasPontuacoes, int *piNome, Memoria memoria[])
 {
     FILE *ranking;
     ranking = fopen("ranking.txt", "r");
@@ -81,41 +35,78 @@ void rankingEmVetor(char *nome, int *pSomaDasPontuacoes, int pontuacao_ante[], i
     while (fgets(buffer, MAX_N_CHAR, ranking))
     {
         recebeBuffer = strtok(buffer, "|");
-        strncpy(memoria[i][0], recebeBuffer, MAX_N_CHAR - 1); // Copia do recebe buffer e passa para memoria
-        memoria[i][0][MAX_N_CHAR - 1] = '\0';                 // Para terminar a string nome;
+        strncpy(memoria[i].nome, recebeBuffer, MAX_N_CHAR - 1); // Copia do recebe buffer e passa para memoria
+        memoria[i].nome[MAX_N_CHAR-1]= '\0';//Acaba a nome na matriz
 
-        recebeBuffer = strtok(NULL, "\n");
-        strncpy(memoria[i][1], recebeBuffer, MAX_N_CHAR - 1); // Copia do recebe buffer e passa para memoria;
-        memoria[i][1][MAX_N_CHAR - 1] = '\0';                 // Para terminar a string  porntuacção;
-        sscanf(memoria[i][1], "%d", &pontuacao_ante[i]);
-
-        if (strcmp(nome, memoria[i][0]) == 0)
-        { // Verificar se o há outras jogadas deste jogador e somar a pontuação dele
-
-            *pSomaDasPontuacoes += pontuacao_ante[i];
-            *pIn = i;
+        recebeBuffer = strtok(NULL, "\n");                   
+        strncpy(memoria[i].pontuacao,recebeBuffer,MAX_N_CHAR-1);  // Copia do recebe buffer e passa para memoria; 
+        memoria[i].pontuacao[MAX_N_CHAR-1] = '\0';  // Para terminar a string  porntuacção;
+                                                               
+        memoria[i].pontos=atoi(memoria[i].pontuacao);
+        
+        if (strcmp(nome, memoria[i].nome) == 0) // Verificar se o há outras jogadas deste jogador e somar a pontuação dele
+        {
+            *pSomaDasPontuacoes += memoria[i].pontos;
+            *piNome = i;
         }
         i++;
     }
-
     fclose(ranking);
 }
 
-void escreveArquivo(int tamanho)
+void organizaRanking(char *nome,int pontuacao_player, int iNome, int *tamanho, Memoria memoria[]){
+    int i = 0;
+    int tam = *tamanho;//Para fazer mudar ou não o tamanho dela para escrever no arquivo
+    Memoria aux1,aux2;
+    
+    //Não queremos sobrepassar a pontuação superiores a nossa e quando acabar ou seja pela pontuação anterior ser menor que a atual ou o indice
+    while (pontuacao_player < memoria[i].pontos && i < *tamanho)
+        i++; 
+
+    aux1 = memoria[i];
+    strncpy(memoria[i].nome, nome, MAX_N_CHAR - 1);
+    sprintf(memoria[i].pontuacao, "%d", pontuacao_player);
+    
+    while (i < *tamanho)
+    {
+        i++;
+        aux2 = memoria[i];
+        memoria[i] = aux1;
+        aux1 = aux2;
+        
+    }
+    
+    if (iNome == -1)
+    {
+         *tamanho = tam + 1;
+         
+    }else{ 
+
+        iNome++;
+        memoria[iNome] = memoria[iNome+1];
+        for (int j = iNome; j <= *tamanho; j++)
+            memoria[j] = memoria[j+1];
+    }
+
+}
+
+
+void escreveArquivo(int tamanho, Memoria memoria[])
 {
     FILE *ranking;
     ranking = fopen("ranking.txt", "w");
 
     for (int i = 0; i < tamanho; i++)
     {
-        fputs(("%s", memoria[i][0]), ranking);
+        fputs(("%s",memoria[i].nome), ranking);
         fputs("|", ranking);
-        fputs(("%s", memoria[i][1]), ranking);
+        fputs(("%s",memoria[i].pontuacao), ranking);
         fputs("\n", ranking);
     }
 
     fclose(ranking);
 }
+
 void leitorArquivo()
 {
     FILE *ranking;
@@ -131,6 +122,7 @@ void leitorArquivo()
 
     fclose(ranking);
 }
+
 int contaLinhasRanking()
 {
     FILE *arquivo;
